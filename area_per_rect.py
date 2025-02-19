87091bb6950c2334c7a6f6bb066cd48ee11169e9
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-02-19 18:08:57 +0000

    area_per_rect.py

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Enoch Amedjrovi
+# Date: feb 18, 2025
+# This program is calculating the area & perimeter of a 5cm and width 3cm.
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm:")
+    print("")
+    print("The area is {}cm^2".format(5 * 3))
+    print("")
+
+
+if __name__ == "__main__":
+    main()
